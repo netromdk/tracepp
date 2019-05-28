f0374d683d34c1f0f68a82b33ebc9bf991631feb
Morten Kristensen <me@mortens.dev>
2019-05-28 19:22:41 +0200

    [cmake] EVERY_WARNING option to enable clang's `-Weverything`

--- cmake/compilation.cmake
+++ cmake/compilation.cmake
@@ -4,3 +4,10 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 if (NOT MSVC)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic-errors")
 endif()
+
+# Clang only
+if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+  if (EVERY_WARNING)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything")
+  endif()
+endif()

5c46b396ea933e8a26daf17cd69647c640676b76
Morten Kristensen <me@mortens.dev>
2025-11-21 18:39:30 +0100

    Fixed -Wunused-result

--- examples/example2.cc
+++ examples/example2.cc
@@ -6,7 +6,7 @@ int main()
   std::vector<std::vector<int>> vec{{1, 2, 3}, {4, 5}, {6, 7, 8}};
   TRACE(vec);
   if (TRACE(vec.size() >= 2)) {
-    TRACE(vec[1]);
+    return TRACE(vec[1])[0];
   }
   return 0;
 }

43e02dd02e90a5c38f29c729edd43406c428c215
Morten Kristensen <me@mortens.dev>
2025-11-28 20:41:51 +0100

    Ensure example print_func returns 0

--- examples/example2.cc
+++ examples/example2.cc
@@ -3,7 +3,7 @@
 
 int main()
 {
-  std::vector<std::vector<int>> vec{{1, 2, 3}, {4, 5}, {6, 7, 8}};
+  std::vector<std::vector<int>> vec{{1, 2, 3}, {0, 5}, {6, 7, 8}};
   TRACE(vec);
   if (TRACE(vec.size() >= 2)) {
     return TRACE(vec[1])[0];
